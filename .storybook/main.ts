7eb1d8f472d5e594306d5ef52668beca43a2d9fd
“Lucas <lucashenrique125@hotmail.com>
2024-06-30 01:57:43 -0300

    Update snapshots for Main component

---
+++ .storybook/main.ts
@@ -0,0 +1,17 @@
+const config = {
+  stories: ['../src/components/**/stories.tsx'],
+  addons: ['@storybook/addon-essentials'],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {}
+  },
+  staticDirs: ['..\\public'],
+  docs: {
+    autodocs: true
+  },
+  webpackFinal: (config) => {
+    config.resolve.modules.push(`${process.cwd()}/src`)
+    return config
+  }
+}
+export default config
